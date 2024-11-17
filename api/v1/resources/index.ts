009989c430230d2b7a3e521cd69aee80a1be20fd
Jesús Agustín Peña Meza <jesusx21.dev@gmail.com>
2024-11-17 17:58:29 -0600

    task: remove unused code

--- api/v1/resources/index.ts
+++ api/v1/resources/index.ts
@@ -1,11 +1,8 @@
 import FilmResource from './film';
-import FilmsResource from './films';
 import SignIn from './signIn';
 import SignUp from './signUp';
 import TVSerieResource from './tvSerie';
 import TVSeriesResource from './tvSeries';
-import WatchlistsResource from './watchlists';
-import { authenticate } from '../middlewares';
 
 class MoviesFreakAPI {
   private app: any;
@@ -16,21 +13,17 @@ class MoviesFreakAPI {
 
   buildAPI() {
     const filmResource = new FilmResource();
-    const filmsResource = new FilmsResource();
     const tvSerieResource = new TVSerieResource();
     const tvSeriesResource = new TVSeriesResource();
-    const watchlistResource = new WatchlistsResource();
 
     const signIn = new SignIn();
     const signUp = new SignUp();
 
-    this.app.registerResource('films', filmsResource);
     this.app.registerResource('films/:filmId', filmResource);
     this.app.registerResource('tv-series', tvSeriesResource);
     this.app.registerResource('tv-series/:tvSerieId', tvSerieResource);
     this.app.registerResource('sign-in', signIn);
     this.app.registerResource('sign-up', signUp);
-    this.app.registerResource('watchlists', watchlistResource, [authenticate])
   }
 }
 
